e46932fd3f723efd820bbd2dcf4060134fd7a74e
patrick012 <jc193950@mail-ljcu.jcu.edu.au>
2021-03-22 18:12:39 +1000

    Added reading, writing and list functionality.

--- CP1404_Pracs/Assignments/reading_tracker.py
+++ CP1404_Pracs/Assignments/reading_tracker.py
@@ -8,7 +8,37 @@ Menu based program, store book lists in file and modify them when needed.
 
 """
 
-INPUT_FILE = "book_file.csv"
+
+def main():
+    display_book_amount()
+    name = input("Please enter your name ")
+    print("Welcome {} ".format(name))
+    print(MENU)
+    input_file = open("book_file.csv", "r")
+    base_list = input_file.readlines()
+    input_file.close()
+
+    menu_choice = input("Please select an option").upper()
+    while menu_choice != "Q":
+        if menu_choice == "L":
+            list_all_books()
+            menu_choice = input("Please select an option").upper()
+        elif menu_choice == "A":
+
+            add_book = str([add_book_to_list()])
+            base_list.extend(add_book)
+            menu_choice = input("Please select an option").upper()
+        elif menu_choice == "M":
+
+            menu_choice = input("Please select an option").upper()
+            file_changes = [mark_as_completed()]
+            base_list[file_changes[1]] = file_changes[0]
+
+    output_file = open("book_file.csv", "w")
+    output_file.writelines(base_list)
+    output_file.close()
+
+
 MENU = "Menu: \n" \
        "L - List all books\n" \
        "A - Add new book\n" \
@@ -25,20 +55,29 @@ def display_book_amount():
     file_length.close()
 
 
-display_book_amount()
-name = input("Please enter your name ")
-print("Welcome {} ".format(name))
-print(MENU)
-menu_choice = input("Please select an option").upper()
-
-if menu_choice == "L":
+def list_all_books():
     book_list = open("book_file.csv", "r")
-    book_list.read()
+    book_list_input = book_list.readlines()
+    book_list_input.sort()
+    pages_sum = 0
+    for i in range(0, len(book_list_input)):
+        title_list = book_list_input[i]
+        title_list_x = title_list.split(",")
+        if title_list_x[3].count("R") >= 1:
+            print("*", i, title_list_x[0], "  by ", end="\t")
+        else:
+            print(i, title_list_x[0], "  by ", end="\t")
+
+        print(title_list_x[1], end="\t")
+        print(title_list_x[2], " pages")
+        if title_list_x[3].count("R") >= 1:
+            pages_sum += int(title_list_x.pop(2))
+    print("Total pages required: ", pages_sum)
     book_list.close()
     print(MENU)
-    menu_choice = input("Please select an option")
 
-elif menu_choice == "A":
+
+def add_book_to_list():
     get_books = open("book_file.csv", "r")
     list_of_books = []
     for line in get_books:
@@ -58,7 +97,27 @@ elif menu_choice == "A":
         pages = int(input("Pages: "))
     for i in range(0, len(list_of_books)):
         print(list_of_books[i])
-
-    book = [title, author, pages, "Required"]
+    book = [title, author, pages, "R"]
     list_of_books.append(book)
     print(book)
+    get_books.close()
+    return list_of_books
+
+
+def mark_as_completed():
+    input_file = open("book_file.csv", "r")
+    try:
+        file_mark = int(input("Which book would you like to mark as completed? "))
+    except ValueError:
+        file_mark = int(input("Which book would you like to mark as completed? "))
+    required_mark = input_file.readlines()
+    print(required_mark[file_mark])
+    removing = required_mark[file_mark]
+    removing_new = removing.replace('R', '', )
+    print(removing_new)
+    required_mark[file_mark] = removing_new
+    input_file.close()
+    return removing_new, file_mark
+
+
+main()
