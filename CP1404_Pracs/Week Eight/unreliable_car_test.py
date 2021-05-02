dd269a5263fbfd133d6daf92ea3111c222c48a29
patrickr012 <jc193950@mail-ljcu.jcu.edu.au>
2021-05-02 18:57:17 +1000

    Initial Commit

---
+++ CP1404_Pracs/Week Eight/unreliable_car_test.py
@@ -0,0 +1,18 @@
+"""
+Patrick Robinson
+12251568
+CP1404
+
+"""
+
+from unreliable_car import UnreliableCar
+
+car1 = UnreliableCar(name="car", fuel=68)
+
+car1.drive(40)
+print(car1)
+
+car2 = UnreliableCar(name="other car", fuel=24)
+
+car2.drive(10)
+print(car2)
